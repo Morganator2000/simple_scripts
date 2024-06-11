b7d5dcede5f896da8761db58634dee4f1b6545c9
Morganator2000 <morganbakelmun@hotmail.com>
2024-06-11 11:27:12 -0400

    Updated to be more human-readable.

--- AbundantTaxa.py
+++ AbundantTaxa.py
@@ -7,21 +7,20 @@ import pandas as pd
 # This script will go through each sample column and determine which taxa and 
 # ASV are the most abundant in a sample.
 
-CODE_TYPE = 'latin1' # https://docs.python.org/3/library/codecs.html#standard-encodings
 input_file = 'feature-table.csv' #Location of the input data. Use a relative path.
 asv_column = '#OTU ID'#Change this if your ASV column is called something else.
 taxa_column = 'Taxon' #Change this if your OTU column is called something else.
 non_samples = [asv_column,taxa_column]
 
-df = pd.read_csv(input_file)
-sample_columns = [col for col in df.columns if col not in non_samples]
+data_frame = pd.read_csv(input_file)
+sample_columns = [col for col in data_frame.columns if col not in non_samples]
 results = []
 
 # Process each sample column
 for column in sample_columns:
     # Find the row with the maximum count for the current sample column
-    max_idx = df[column].idxmax()
-    max_row = df.loc[max_idx]
+    max_index = data_frame[column].idxmax()
+    max_row = data_frame.loc[max_index]
     
     # Append the sample name, ID, and Animal to the results list
     results.append({
@@ -30,8 +29,6 @@ for column in sample_columns:
         asv_column: max_row[asv_column]
     })
 
-# Create a new DataFrame from the results list
+# Create abd export the results data frame
 results_df = pd.DataFrame(results)
-
-# Write the results DataFrame to a new CSV file
 results_df.to_csv('results.csv', index=False)
