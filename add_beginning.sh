ca5dde1a1f1eb093b1e620d583b4f1b81ddab17f
Morganator2000 <morganbakelmun@hotmail.com>
2024-06-25 15:53:52 -0400

    Ready to test

--- add_beginning.sh
+++ add_beginning.sh
@@ -15,37 +15,53 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-#the first and only argument, the file name.
-file=$1
+#the first and only argument, the script name.
+script_file=$1
 
-#step 1, make sure that the argument is a valid extension.
+#step 1, make sure that the argument is a valid script.
 if [ ! -f "$script_file" ]; then
   echo "Error: File '$script_file' not found."
   exit 1
 fi
 
-if [[ "$file" != *.sh && "$file" != *.txt ]]; then
+if [[ "$script_file" != *.sh && "$script_file" != *.txt ]]; then
 	echo "Invalid file extension. You can only add this to .sh and .txt files"
 	exit 1
 fi
-#TODO: verify that the SBATCH lines aren't already there. If they are, remove them.
+#step 2, make an empty file for storing the new script.
+temp_file=$(mktemp)
 
-#step 2, save everything that is already written in the script.
+#step 3, if the SBATCH lines already exist, remove them first.
+# Flag to indicate whether we are done skipping SBATCH lines
+skip_done=false
+
+while IFS= read -r line; do
+  if ! $skip_done; then
+    if [[ "$line" == *"SBATCH"* || "$line" == "#!/bin/"* ]]; then
+      continue
+    fi
+    skip_done=true
+  fi
+  echo "$line" >> "$temp_file"
+done < "$script_file"
+
 info=$(cat $1)
 
-#step 3, add SBATCH lines. Note that this temporarily removes the file contents.
+#step 3, add SBATCH lines, then re-add the script.
+{
+	echo "#!/bin/bash -l"
+	echo "#SBATCH --job-name=$(basename 'script_file')"
+	#TODO: output is job-name + .out
+	echo "#SBATCH --output=$(basename 'script_file').out"
+	echo "#SBATCH --partition=standard"
+	echo "#SBATCH --account=grdi_genarcc"
+	echo "#SBATCH --time=12:00:00"
+	echo "#SBATCH --ntasks=1"
+	echo "#SBATCH --cpus-per-task=1"
+	echo "#SBATCH --comment=\"Submitted job\"" # Probably should change the comment.
+	echo
+	cat "$temp_file"
+} > "$script_file"
 
-echo "#!/bin/bash -l" > $file
-#TODO: job name is script name, excluding extension
-echo "#SBATCH --job-name=" >> $file
-#TODO: output is job-name + .out
-echo "#SBATCH --output=" >> $file
-echo "#SBATCH --partition=standard" >> $file
-echo "#SBATCH --account=grdi_genarcc"  >> $file
-echo "#SBATCH --time=12:00:00" >> $file
-echo "#SBATCH --ntasks=1" >> $file
-echo "#SBATCH --cpus-per-task=1" >> $file
-echo "#SBATCH --comment=\"Submitted job\"" >> $file # Probably should change the comment.
+rm $temp_file
 
-#step 4, re-add the things written in the script.
-echo -e "\n$info" >> $file
