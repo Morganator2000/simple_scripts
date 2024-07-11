66a9f6717db68c40eda96e54d7981ad3b339a49a
Morganator2000 <morganbakelmun@hotmail.com>
2024-07-11 10:33:38 -0400

    Changes default arguments

--- blank_SBATCH.sh
+++ blank_SBATCH.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=$(basename "script_file")
-#SBATCH --output=$(basename "script_file").out
+#SBATCH --job-name=script
+#SBATCH --output=script.out
 #SBATCH --partition=standard
 #SBATCH --account=grdi_genarcc
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --comment=\"Submitted job\"
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --comment="Submitted job"
 
