b0affd7f9ce65e402da04fd966b6d5eaf899474d
Morganator2000 <morganbakelmun@hotmail.com>
2024-08-29 14:39:17 -0400

    Small fix

--- CheckDuplicates.py
+++ CheckDuplicates.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 def check_duplicates(df):
     duplicate_rows = df[df.duplicated(keep=False)]
-    return duplicate_rows
+    if duplicate_rows.empty:
+        return "No duplicate rows found"
+    else:
+        return duplicate_rows
 
-input_file = 'Manifest_Bac16S_list.csv'
+input_file = 'file.csv'
 data_frame = pd.read_csv(input_file)
 
 duplicates = check_duplicates(data_frame)
